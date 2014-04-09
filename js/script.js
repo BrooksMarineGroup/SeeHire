823548261e28f2389e9ba90a7af2c5d4acda04e2
Bryan Jensen <bryanawjensen@gmail.com>
2014-04-08 23:25:27 -0400

    Moving things around again - also got jquery animated scrolling happening

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// $(document).foundation();
+$(function() {
+	$('a').click(function() {
+		// console.log("Yay");
+		$('html body').animate({
+			scrollTop: $( this.hash ).offset().top
+		}, 500);
+
+		return false;
+	});
+});
