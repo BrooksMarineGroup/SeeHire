31e5ab1a6cc6b8dcfe4cd8eaa93f01e146a9b8f4
Bryan Jensen <bryanawjensen@gmail.com>
2014-04-10 21:15:00 -0400

    Attempted something with banded.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function() {
-	$('a').click(function() {
+	$('a').click(function(event) {
+		event.preventDefault();
+		
 		$('html body').animate({
 			scrollTop: $( this.hash ).offset().top - 50
 		}, 500);
